14b5e4984eafccf7abce509d7c5a5261ae24a2da
Alex Tsiras <alextsiras@gmail.com>
2018-08-15 21:17:34 +0300

    Fix command worker

--- app/workers/command_worker.rb
+++ app/workers/command_worker.rb
@@ -1,15 +1,18 @@
+require 'httparty'
+
 class CommandWorker
   include HTTParty
   include Sidekiq::Worker
   sidekiq_options :retry => false
 
   def perform(params)
+    params = params.symbolize_keys
     message = {
       text: "You just sent #{params[:text]}",
       response_type: "in_channel"
     }
     puts "----------------------------------------"
-    puts "Message: ", params[:text]
+    puts "Message: ", params.inspect
     puts "----------------------------------------"
     HTTParty.post(params[:response_url], { body: message.to_json, headers: {
         "Content-Type" => "application/json"
