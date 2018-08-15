b5871e02e73f69928e54888748e9eb483373e9ad
Alex Tsiras <alextsiras@gmail.com>
2018-08-15 19:35:00 +0300

    Change to markd command

--- app/slack/md_command.rb
+++ app/slack/md_command.rb
@@ -1,3 +1,14 @@
-class  MdCommand < Slackathon::Command
+class  MarkdCommand < Slackathon::Command
+  def call
+    {
+      response_type: "in_channel",
+      text: "#{user} said #{params[:text]} :see_no_evil:"
+    }
+  end
 
+  private
+
+  def user
+    "<@#{params[:user_id]}>"
+  end
 end
